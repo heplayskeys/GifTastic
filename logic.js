dffa7d89ae731f4c562f2000d3e1a2d8474e6255
heplayskeys <heplayskeys@gmail.com>
2018-12-06 23:14:56 -0800

    Completed HW. Ready to submit. Room for improvement.

--- logic.js
+++ logic.js
@@ -1,5 +1,11 @@
 // Global Variables
     var colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+    var favGIFs = [];
+    var favCheck = [];
+    var imgURLs = [];
+    var userColor;
+    var addMore = 0;
+    var imgLimit = 10;
 
 // *********************************
 
@@ -39,57 +45,191 @@
     function displayGIFs() {
 
         $("#GIFview").empty();
+        $(".GIFclick").empty();
+        $("#note").show();
+        addMore = 0;
+        imgLimit = 10;
 
         var color = $(this).attr("data-color");
         var APIKey = "Lk6iVDZN01o2tWotu99YM9UMk6I4BAC2";
         var queryURL = "https://api.giphy.com/v1/gifs/search?limit=10&api_key=" + APIKey + "&q=" + color + "+funny";
+            userColor = color;
 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
+
             console.log(response);
 
-            var animated = [];
-            var static = [];
+            var results = response.data;     
 
-            for (var i = 0; i < response.data.length; i++) {
+            for (var i = 0; i < results.length; i++) {
+
+            var staticImgUrl = results[i].images.fixed_height_still.url;
+            var animatedImgUrl = results[i].images.fixed_height.url;
             var GIFcontainer = $("<div>").addClass("GIFclick");
-            var GIFrating = $("<h3>").html("Rating: " + response.data[i].rating);
-            var GIFstaticIMG = $("<img>").attr("src", response.data[i].images.fixed_height_still.url).addClass("static").attr("data-index", i);
-            
-            GIFcontainer.append(GIFrating, GIFstaticIMG);
+            var GIFimportDate = $("<h4>").html("Date Added: " + results[i].import_datetime.slice(0, 10));
+            var GIFrating = $("<h3>").html("Rating: " + results[i].rating);
+            var GIFtitle = $("<h2>").html(results[i].title.substr(0, results[i].title.indexOf("GIF"))).addClass("GIFtitle");
+            var GIFimage = $("<img>").attr("src", staticImgUrl).addClass("static").attr("data-static", staticImgUrl).attr("data-animate", animatedImgUrl).attr("data-index", i);
+            var newLine = $("<p>");
+            // It seems there are access restrictions to downloading GIF images from GIPHY -- Perhaps I just can't figure out the correct link to use or syntax
+            var GIFdownload = $("<a>").attr("href", animatedImgUrl).attr("download", "results[i].images.original.url.substr(8)").html("Download");
+            var favoriteBTN = $("<img>").addClass("makeFav").attr("src", "assets/images/heart.png").attr("data-loc", i);
+       
+            newLine.append(GIFdownload).append(favoriteBTN);
+            GIFcontainer.append(GIFtitle, GIFrating, GIFimportDate, newLine, GIFimage);
 
             $("#GIFview").append(GIFcontainer);
-
-            animated.push(response.data[i].images.fixed_height.url);
-            static.push(response.data[i].images.fixed_height_still.url);
             }
 
-            console.log(animated);
-            console.log(static);
-
-                $(document).on("click", ".static", function() {
-                $(this).removeClass("static").addClass("animate");                
-            var indexVal = $(this).attr("data-index");
-                $(this).attr("src", animated[indexVal]).css("filter", "grayscale(0%)");
-                console.log(this);
+                // Click GIF to Animate
+                $(".container").on("click", ".static", function() {              
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).removeClass("static").addClass("animate");  
             });
-
-                $(document).on("click", ".animate", function() {
-                $(this).removeClass("animate").addClass("static");                
-            var indexVal = $(this).attr("data-index");
-                $(this).attr("src", static[indexVal]).css("filter", "grayscale(100%)");
-                console.log(this);
+                // Click to Stop
+                $(".container").on("click", ".animate", function() {              
+                $(this).attr("src", $(this).attr("data-static"));
+                $(this).removeClass("animate").addClass("static");  
             });
             
+            // Click Button to Stop All
+            var stopButton = $("<button>");
+            stopButton.text("Click to Stop All").addClass("stopButton");
+            $("#stopBtn").html(stopButton);
+            $(".container").on("click", ".stopButton", function() {
+                $("img").removeClass("animate").addClass("static").attr("src", function() {
+                    return $(this).attr("data-static");
+                });
+            });
+
+            // Click to Add Favorite
+            $(".makeFav").on("click", function() {
+                
+                var favIndex = $(this).attr("data-loc");
+                var favUrl = results[favIndex].images.fixed_height_small.url;
+                var origURL = results[favIndex].images.original.url;
+    
+                if ($.inArray(favUrl.substr(24), favCheck) === -1) {
+    
+                    $("#favorites").empty();
+    
+                    favGIFs.push(favUrl);
+                    favCheck.push(favUrl.substr(24));
+                    imgURLs.push(origURL);
+    
+                    for (var i = 0; i < favGIFs.length; i++) {
+                        var favContainer = $("<div>").addClass("favGIF");
+                        var favDownload = $("<a>").attr("href", imgURLs[i]).attr("download", imgURLs[i]).attr("target", "_blank");
+                        var favGIFimage = $("<img>").attr("src", favGIFs[i]).attr("data-fav", i);
+    
+                        favDownload.append(favGIFimage);
+                        favContainer.append(favDownload);
+                        
+                        $("#favorites").append(favContainer);
+
+                        var clearButton = $("<button>");
+                        clearButton.text("Click to Clear Favorites").addClass("clearButton");
+                        $("#clearBtn").html(clearButton);
+                    }
+                }
+            });
+
+            // Click Button to Clear Favorites
+            $(".container").on("click", ".clearButton", function() {
+                $("#favorites").empty();
+                $("#clearBtn").empty();
+                favGIFs = [];
+                favCheck = [];
+                imgURLs = [];
+            });
         });
+    }
+
+    function reset() {
+        colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+        renderButtons();
     }
 
 // *********************************
 
 // Main Page Function
-$(document).on("click", ".color", displayGIFs);
-
+$(".container").on("click", ".color", displayGIFs);
+$(".container").on("click", "#reset", reset);
+$("#note").hide();
 
 renderButtons();
+
+// Bonus
+$("#add-more").on("click", function(event) {
+
+        event.preventDefault();
+        addMore += 10;
+        imgLimit += 10;
+
+        var APIKey = "Lk6iVDZN01o2tWotu99YM9UMk6I4BAC2";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?limit=" + imgLimit + "&api_key=" + APIKey + "&q=" + userColor + "+funny";
+            
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+
+            var results = response.data;     
+
+            for (var i = addMore; i < results.length; i++) {
+
+                var staticImgUrl = results[i].images.fixed_height_still.url;
+                var animatedImgUrl = results[i].images.fixed_height.url;
+                var GIFcontainer = $("<div>").addClass("GIFclick");
+                var GIFimportDate = $("<h4>").html("Date Added: " + results[i].import_datetime.slice(0, 10));
+                var GIFrating = $("<h3>").html("Rating: " + results[i].rating);
+                var GIFtitle = $("<h2>").html(results[i].title.substr(0, results[i].title.indexOf("GIF"))).addClass("GIFtitle");
+                var GIFimage = $("<img>").attr("src", staticImgUrl).addClass("static").attr("data-static", staticImgUrl).attr("data-animate", animatedImgUrl).attr("data-index", i);
+                var newLine = $("<p>");
+                // It seems there are access restrictions to downloading GIF images from GIPHY -- Perhaps I just can't figure out the correct link to use or syntax
+                var GIFdownload = $("<a>").attr("href", animatedImgUrl).attr("download", "results[i].images.original.url.substr(8)").html("Download");
+                var favoriteBTN = $("<img>").addClass("makeFav").attr("src", "assets/images/heart.png").attr("data-loc", i);
+           
+                newLine.append(GIFdownload).append(favoriteBTN);
+                GIFcontainer.append(GIFtitle, GIFrating, GIFimportDate, newLine, GIFimage);
+    
+                $("#GIFview").prepend(GIFcontainer);
+            }
+
+        // Click to Add Favorite
+        $(".makeFav").on("click", function() {
+                
+            var favIndex = $(this).attr("data-loc");
+            var favUrl = results[favIndex].images.fixed_height_small.url;
+            var origURL = results[favIndex].images.original.url;
+
+            if ($.inArray(favUrl.substr(24), favCheck) === -1) {
+
+                $("#favorites").empty();
+
+                favGIFs.push(favUrl);
+                favCheck.push(favUrl.substr(24));
+                imgURLs.push(origURL);
+
+                for (var i = 0; i < favGIFs.length; i++) {
+                    var favContainer = $("<div>").addClass("favGIF");
+                    var favDownload = $("<a>").attr("href", imgURLs[i]).attr("download", imgURLs[i]).attr("target", "_blank");
+                    var favGIFimage = $("<img>").attr("src", favGIFs[i]).attr("data-fav", i);
+
+                    favDownload.append(favGIFimage);
+                    favContainer.append(favDownload);
+                    
+                    $("#favorites").append(favContainer);
+
+                    var clearButton = $("<button>");
+                    clearButton.text("Click to Clear Favorites").addClass("clearButton");
+                    $("#clearBtn").html(clearButton);
+                }
+            }
+        });
+    });
+});
